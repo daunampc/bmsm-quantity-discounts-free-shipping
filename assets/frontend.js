bd7f68d06e68b1e6121df1d7270b43872d66e4bc
ToshStack <133574126+daunampc@users.noreply.github.com>
2026-06-08 10:54:11 +0700

    Move offer boxes below add to cart button

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var productCartForm = document.querySelector('form.cart');
+    var offerBox = document.querySelector('.single-product .bmsm-qdfs-offers');
+
+    if (productCartForm && offerBox) {
+        productCartForm.insertAdjacentElement('afterend', offerBox);
+    }
+});
+
 document.addEventListener('click', function(event) {
     var copyButton = event.target.closest('.bmsm-qdfs-copy');
     if (copyButton) {
